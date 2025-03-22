804ff1216e79d3efb95d656b301d2aea548e1f70
sonnenbarke <pelikanol@proton.me>
2025-03-22 20:44:55 +1100

    Adding more files for js code aand loading data code

---
+++ js/load-data.js
@@ -0,0 +1,15 @@
+d3.csv("../data/chinameat.csv", row => ({
+    year: +row.year,
+    beef: +row.beef,
+    pig: +row.pig,
+    poultry: +row.poultry,
+    sheep: +row.sheep
+  })).then(data => {
+    console.log("Parsed data:", data); // Check if 'year' is being parsed correctly
+    defineScales(data);
+    drawDonutCharts(data);
+    drawStackedBars(data);
+    drawStreamGraph(data);
+    addLegend();
+  });
+  
---
+++ js/shared-constant.js
@@ -0,0 +1,13 @@
+const margin = {top: 50, right: 0, bottom: 50, left: 70};
+const width = 900;
+const height = 350;
+const innerWidth = width - margin.left - margin.right;
+const innerHeight = height - margin.top - margin.bottom;
+
+const formatsInfo = [
+  {id: "beef", label: "Beef", color: "#76B6C2"},
+  {id: "pig", label: "Pork", color: "#4CDDF7"},
+  {id: "poultry", label: "Poultry", color: "#ED7864"},
+  {id: "sheep", label: "Sheep", color: "#2F8999"},
+];
+
